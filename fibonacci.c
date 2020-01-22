e15959dbda50623796122be1b42eecc1fd9beda4
MAb1ga1l <49538639+MAb1ga1l@users.noreply.github.com>
2020-01-21 18:08:36 -0600

    Update fibonacci.c
    ya funcional la serie de fibonacci

--- fibonacci.c
+++ fibonacci.c
@@ -6,44 +6,36 @@
 //
 
 #include "fibonacci.h"
-float fi_iter(int n);
-float fi_re(int n, int c,int a);
+long long int fi_iter(int n);
+long long int fi_re(long long int n,long long int c,long long int a);
 
 int main(void){
-    int n=8;
-    float fibonacci1=0,fibonacci2=0;
+    clock_t start_t, end_t, total_t;
+    int n=10;
+    long long int fibonacci1,fibonacci2;
     fibonacci1=fi_iter(n);
-    printf("resultado iter = %f \n",fibonacci1);
+    printf("resultado iter = %lld \n",fibonacci1);
     fibonacci2=fi_re(n,1,0);
-    printf("resultado recur = %f \n",fibonacci2);
-
+    printf("resultado recur = %lld \n",fibonacci2);
 }
 
-float fi_iter(int n){
-  clock_t start_t, end_t, total_t;
-  start_t = clock();
-    int a=1,b=0,c=0;
-    for(int i=2;i<=n;i++){
+long long int fi_iter(int n){
+    long long int a=1,b=0,c=0;
+    for(int i=1;i<=n;i++){
         c=a+b;
         a=b;
         b=c;
     }
-    end_t=clock();
-    total_t=(double)(end_t - start_t) / CLOCKS_PER_SEC;
-    return total_t;
+    return c;
 }
 
 
-float fi_re(int n, int c, int a){
-  clock_t start_t, end_t, total_t;
-  start_t = clock();
-  int b=0;
+long long int fi_re(long long int n,long long  int c,long long  int a){
+  long long int b=0;
   b=c+a;
-  if(n>0){
+  if(n>=0){
     n--;
     c=fi_re(n,a,b);
   }
-  end_t=clock();
-  total_t=(double)(end_t - start_t) / CLOCKS_PER_SEC;
-  return total_t;
+  return c;
 }
